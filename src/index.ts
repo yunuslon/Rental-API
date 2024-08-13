90f49c1b9c36888a55204615e987445a0bda2068
myunus <m.yunus@dxtr.asia>
2024-08-13 10:39:00 +0700

    feat: add template

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import customerRoute from "./routes/customers";
+const path = require("path");
 
 const app = express();
+
 app.use(express.json());
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+
 app.use("/api/v1", customerRoute);
+
+app.get("/", (req, res) => {
+  res.render("pages/signIn");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
